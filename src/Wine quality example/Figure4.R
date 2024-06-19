3d412d1c8b1cb3d30fb6d25769a5629a95ca3854
xpzong <xpzong@bjut.edu.cn>
2024-06-19 20:11:14 +0800

    Add files via upload

---
+++ src/Wine quality example/Figure4.R
@@ -0,0 +1,35 @@
+library(readxl)
+Wine_example_mean <- read_excel("C:/Users/dell/Desktop/2023.0257/scr/output/Wine_example_mean.xlsx",col_names = FALSE)
+#Wine_example_std <- read_excel("C:/Users/dell/Desktop/2023.0257/scr/output/Wine_example_std.xlsx",col_names = FALSE)
+
+
+par(family = 'Kai')  
+# mean
+## TPL
+a1=as.numeric(Wine_example_mean[1,1:12])
+a2=as.numeric(Wine_example_mean[2,1:12])
+## SPL
+#a1=as.numeric(Wine_example_mean[3,1:12])
+#a2=as.numeric(Wine_example_mean[4,1:12])
+
+# std
+## TPL
+#a1=as.numeric(Wine_example_std[1,1:12])
+#a2=as.numeric(Wine_example_std[2,1:12])
+## SPL
+#a1=as.numeric(Wine_example_std[3,1:12])
+#a2=as.numeric(Wine_example_std[4,1:12])
+
+data=as.matrix(rbind(a1,a2),2,12)
+barplot(height = data,  
+        names.arg = c('Full','AIC','BIC','SAIC','SBIC','MMA','Equal','K-data','RMA-op', 'RMA-2','RMA-5','RMA-n/2'), 
+        family = 'Kai',  
+        col = c('red', 'blue'), 
+        border = '#ffffff', 
+        ylab = 'Relative Targeted Empirical Ranking Risk',  
+        horiz = FALSE,  
+        ylim = c(0, 2),
+        legend.text = c('Red Wine', 'White Wine'),  
+        beside = TRUE 
+)
+
