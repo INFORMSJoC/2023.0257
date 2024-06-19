3c6720ac854b63912f1e606ca90cf82f34547674
xpzong <xpzong@bjut.edu.cn>
2024-06-19 20:40:26 +0800

    Add files via upload

---
+++ src/Supplement/SimA7/SimuA7_20/order.R
@@ -0,0 +1,31 @@
+library(R.matlab)
+library("lbfgs")
+library("survPresmooth")
+library("MASS")
+library("mvtnorm")
+library("survival")
+library("ncvreg")
+library("doParallel")
+library("foreach")
+library("glmnet")
+source("Gen_Dat.R")
+source("Fun.R")
+source("screen.R")
+source("method.R")
+# 设置为该函数所在的路径
+setwd("C:/Users/dell/Desktop/2023.0257/scr/Supplement/SimA7/SimuA7_20")
+
+# 输入参数
+inipara <- readMat("para.mat");
+
+
+X = inipara$x.sc
+Y = inipara$y.sc
+
+Delta = inipara$Delta.sc
+
+stat.scr = IPOD(X,Delta,Y,gamma=0.8)
+or = c(order(stat.scr, decreasing = T))
+
+
+writeMat("screen.mat",r_ans=or)
