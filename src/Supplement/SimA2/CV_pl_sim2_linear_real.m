a1d41e57f4b4b593878d5b2a0b714852cd0bef5c
xpzong <xpzong@bjut.edu.cn>
2024-06-19 20:18:58 +0800

    Add files via upload

---
+++ src/Supplement/SimA2/CV_pl_sim2_linear_real.m
@@ -0,0 +1,24 @@
+% RMA targeted PL cross-validation
+function [L_n] = CV_pl_sim2_linear_real(x,y,K,idx,n0,n_K,M,w)
+
+L_k=zeros(K,1);
+predict_all=zeros(n0,M);
+for k=1:K
+    idx_k=(idx~=k);
+    x_k=x(idx_k,:);
+    y_k=y(idx_k);
+    idx_k0=(idx==k);
+    x_k0=x(idx_k0,:);
+    y_k0=y(idx_k0);    
+    for m=1:M
+        x_km=x_k(:,1:m);
+        x_k0m=x_k0(:,1:m);
+        beta_km=(x_km'*x_km)\(x_km'*y_k); 
+        predict_all(idx_k0,m)=x_k0m*beta_km;            
+    end 
+    L_k(k)=pairwise_loss_real(n_K,y_k0,predict_all(idx_k0,:),w); 
+end
+
+L_n=sum(L_k)/K;
+
+end
---
+++ src/Supplement/SimA2/pairwise_loss_approximate_Annals_2.m
@@ -0,0 +1,34 @@
+function [fun] = pairwise_loss_approximate_Annals_2(n,y,predict_all,w)
+
+% surrogate pairwise loss function 2
+fun=0;
+for j=1:n
+    for i=1:n
+        if i~=j    
+            m_i=predict_all(i,:);                     %第i个个体的预测值
+            y_i=y(i);                                 %第i个个体的真实值
+            m_j=predict_all(j,:);                     %第j个个体的预测值
+            y_j=y(j);                                 %第j个个体的真实值
+            zij=(y_i-y_j)/2;
+            if zij==0
+                sgn_zij=0;
+            elseif zij~=0
+                sgn_zij=zij/abs(zij);
+            end
+            inner_function=m_i-m_j;                       %从内往外第一个函数
+            outter_function=-w*(sgn_zij*inner_function)';  %从内往外第二个函数
+            if (1+outter_function)>=0
+                psi=1+outter_function;
+            elseif (1+outter_function)<0
+                psi=0;
+            end
+            fun=fun+psi;
+        elseif i==j
+            fun=fun+0;
+        end
+    end
+end
+
+fun=(fun/(n*(n-1)));
+
+end
